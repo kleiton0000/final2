0ea085ffeeedb30d5a5cc062ec39b75eeb40562e
kleiton0000 <137412765+kleiton0000@users.noreply.github.com>
2024-09-25 10:29:57 -0300

    A1

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2025-11-05T00:21:20");
+const tempoObjetivo2 = new Date("2027-15-05T00:24:26");
+const tempoObjetivo3 = new Date("2028-18-30T00:28:39");
+const tempoObjetivo4 = new Date("2029-05-01T00:50:57");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
